e865e4b087d1ab517bb62b821d494ee88e92be06
Nansen Ho <song.he0302@gmail.com>
2023-10-21 14:52:44 +0900

    xiaohongshu - iife & closure

---
+++ docs/javascript/base/window/xiaohongshu_access_min_window/main_iife.js
@@ -0,0 +1,43 @@
+const handlePopupWindowClick = (function () {
+  let popupWindow = null;
+  return function () {
+    if (isWindowClosedOrNotInitialized(popupWindow)) {
+      popupWindow = openNewPopupWindow();
+    } else {
+      focusOnPopupWindow(popupWindow);
+    }
+  };
+})();
+
+const openNewPopupWindow = (function () {
+  const popupWindowFeatures = {
+    left: 100,
+    top: 100,
+    width: 430,
+    height: 1200,
+  };
+  return function () {
+    const url = "https://www.xiaohongshu.com/explore";
+    const name = "xiaohongshu";
+    const featuresString = formatFeaturesToString(popupWindowFeatures);
+    return window.open(url, name, featuresString);
+  };
+})();
+
+function isWindowClosedOrNotInitialized(window) {
+  return window === null || window.closed;
+}
+
+function focusOnPopupWindow(window) {
+  window.focus();
+}
+
+function formatFeaturesToString(features) {
+  return Object.entries(features)
+    .map(([key, value]) => `${key}=${value}`)
+    .join(",");
+}
+
+document
+  .querySelector("#access-min-window")
+  .addEventListener("click", handlePopupWindowClick, false);
