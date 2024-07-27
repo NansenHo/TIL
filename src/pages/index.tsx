be09050c2bb42b70380116713dda4125b25dd310
Nansen Ho <song.he0302@gmail.com>
2024-07-27 15:37:36 +0900

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
-import React from "react";
-import { Redirect } from "@docusaurus/router";
+import { useEffect } from "react";
+import { useHistory } from "@docusaurus/router";
 
-export default function Home(): JSX.Element {
-  return <Redirect to="/docs/" />;
+export default function Home() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push("/docs");
+  }, [history]);
+
+  return null;
 }
